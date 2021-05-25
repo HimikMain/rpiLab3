7721e3cf1b51cc73f78526b9578845207069c52c
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-05-25 15:07:05 +0300

    Русский для списка писателей

---
+++ lang/locale/ru/ruIndex2.js
@@ -0,0 +1,12 @@
+document.querySelector('Title').text = "Писатели Беларуси";
+
+document.querySelector('.first, .nav_link').text = "Главная страница";
+document.querySelector('.nav_link:nth-of-type(2)').text = "Список писателей";
+
+document.querySelector('.searchPis label').innerHTML = "Поиск писателей";
+
+document.querySelector('.first, .writers a').innerText = "Якуб Колас";
+document.querySelector('.writers:nth-of-type(2) a').innerHTML = "Янка Купала";
+document.querySelector('.writers:nth-of-type(3) a').innerHTML = "Владимир Короткевич";
+document.querySelector('.writers:nth-of-type(4) a').innerHTML = "Светлана Алексиевич";
+document.querySelector('.writers:nth-of-type(5) a').innerHTML = "Максим Танк";
